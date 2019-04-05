b9a05fb6625d61b8d2647da5d3a57113804d07dc
Micaela Jawor <micaelajawor@yahoo.com>
2019-04-05 15:30:38 -0700

    work on app.component

--- site-rebuild/src/app/app.component.ts
+++ site-rebuild/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Super Cool DIY';
+  firstPin: Pin = new Pin("How to make a miniature fimo succulent");
+  }
+}
+export class Pin {
+  public done: boolean = false;
+  constructor(public description: string) { }
 }
